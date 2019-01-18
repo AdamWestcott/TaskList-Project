77e296765318c30e5dd0745b9e87e98b9c6ab0c6
Adam Westcott <k1746252@kingston.ac.uk>
2019-01-18 12:31:01 +0000

    Moving Test1 and test 2 from default package to test package.

---
+++ TigerProject/src/main/java/TestPackage/test2.java
@@ -0,0 +1,15 @@
+package TestPackage;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author k1732912
+ */
+public class test2 {
+    
+}
