f59012b93dee6e6fd47b05d6a4a5913063c5c5f4
Lewys Richards <k1732912@kingston.ac.uk>
2019-02-25 22:43:55 +0000

    Added a very, very basic gui

--- TigerProject/src/main/java/com/tiger/todolist/view/BoardView.java
+++ TigerProject/src/main/java/com/tiger/todolist/view/BoardView.java
@@ -5,10 +5,46 @@
  */
 package com.tiger.todolist.view;
 
+    import javax.swing.JFrame;
+    import javax.swing.JLabel;
+    import java.awt.FlowLayout;
+    import javax.swing.JButton;
+    import javax.swing.JCheckBox;
+    import javax.swing.JComboBox;
+    import javax.swing.JRadioButton;
+    import javax.swing.JTextField;
+    import javax.swing.WindowConstants;
+
 /**
  *
  * @author Lewys
  */
 public class BoardView {
-    
+    public static void main(String[] args){
+        
+        JFrame testWindow = new JFrame("Test: To Do List!");     // Create top level window
+        testWindow.setLayout(new FlowLayout());                  // Set the layout of the window
+
+        JLabel task = new JLabel("Enter task:");                 // Create components // Label
+        JTextField taskBox = new JTextField("Enter task");       // Text Field
+        JCheckBox completeBox = new JCheckBox("Complete");       // Checkbox
+        JRadioButton radioButtonExample = new JRadioButton("Radio Button ex");  // Radio Button
+        
+        String dropDownList[] = {"A","Drop","Down","List","Example"};
+        JComboBox comboBox = new JComboBox(dropDownList);        // Combo list - takes an array
+        
+        JButton button = new JButton("Maybe we could use this to add a task?"); // Button
+        
+        testWindow.add(task);
+        testWindow.add(taskBox);
+        testWindow.add(completeBox);
+        testWindow.add(radioButtonExample);
+        testWindow.add(comboBox);
+        testWindow.add(button);
+        
+        testWindow.pack();
+        
+        testWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//Closing operation 
+        testWindow.setVisible(true);                                    //Making window visible
+    }
 }
