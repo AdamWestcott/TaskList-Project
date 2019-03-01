166d9b27c270f04fff5a27ef1f3160fc770b8317
Adam Westcott <k1746252@kingston.ac.uk>
2019-03-01 10:03:56 +0000

    Created a class called ListBoard which extends JPanel

---
+++ TigerProject/src/main/java/com/tiger/todolist/view/ListBoard.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tiger.todolist.view;
+
+/**
+ *
+ * @author k1746252
+ */
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class ListBoard {
+private JLabel list= new JLabel ("Lists");  
+private JTextArea lists = new JTextArea();
+
+}
