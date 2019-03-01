fa016956fe6f4c40f523408f0e680c15cc28968e
Adam Westcott <k1746252@kingston.ac.uk>
2019-03-01 10:31:52 +0000

    Created a constructor for ListBoard

--- TigerProject/src/main/java/com/tiger/todolist/view/ListBoard.java
+++ TigerProject/src/main/java/com/tiger/todolist/view/ListBoard.java
@@ -10,13 +10,41 @@ package com.tiger.todolist.view;
  * @author k1746252
  */
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-public class ListBoard {
-private JLabel list= new JLabel ("Lists");  
-private JTextArea lists = new JTextArea();
+public class ListBoard extends JPanel {
+
+    private JLabel list= new JLabel ("Lists");  
+    private JTextArea lists = new JTextArea();
+    
+    public JLabel getList() {
+        return list;
+    }
+
+    public void setList(JLabel list) {
+        this.list = list;
+    }
+
+    public JTextArea getLists() {
+        return lists;
+    }
+
+    public void setLists(JTextArea lists) {
+        this.lists = lists;
+    }
+
+    public ListBoard()
+    {
+        this.setLayout(new FlowLayout());
+        JScrollPane scroll =new JScrollPane(lists);
+        this.add(scroll,BorderLayout.CENTER);
+        this.add(list);
+        lists.setLineWrap(true);
+        lists.setEditable(false);
+    }
 
 }
