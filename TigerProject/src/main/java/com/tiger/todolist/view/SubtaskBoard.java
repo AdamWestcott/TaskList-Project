f7dbce682a03c222d67f52420e1a68dc78180077
Lewys Richards <K1732912@kingston.ac.uk>
2019-03-01 10:04:04 +0000

    Added sub board JPanel.

---
+++ TigerProject/src/main/java/com/tiger/todolist/view/SubtaskBoard.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tiger.todolist.view;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author k1732912
+ */
+public class SubtaskBoard extends TaskBoard{
+    private JLabel subTasks = new JLabel("Subtasks");
+    
+}
