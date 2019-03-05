39227cf81c20f6376501c497a68989e3c7922aea
Lewys Richards <K1732912@kingston.ac.uk>
2019-03-05 14:27:37 +0000

    Moved main.class out of packages.

---
+++ TigerProject/src/main/java/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import  com.tiger.todolist.view.mainWindow;
+import  com.tiger.todolist.view.taskWindow;
+import  com.tiger.todolist.view.subtaskWindow;
+import java.util.Date;
+
+/**
+ *
+ * @author K1732912
+ */
+public class Main {
+
+    public static void main(String[] args) {
+    
+       // mainWindow a = new mainWindow();
+        //taskWindow b = new taskWindow();
+        subtaskWindow c = new subtaskWindow(); 
+    }
+    
+}
