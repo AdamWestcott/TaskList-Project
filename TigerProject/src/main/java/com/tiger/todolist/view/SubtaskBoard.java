a3b0bd6d7f71034d9c4a01fd1c5fce37bd4dc3e4
Adam Westcott <k1746252@kingston.ac.uk>
2019-03-26 16:01:25 +0000

    Populated subtask view

--- TigerProject/src/main/java/com/tiger/todolist/view/SubtaskBoard.java
+++ TigerProject/src/main/java/com/tiger/todolist/view/SubtaskBoard.java
@@ -5,7 +5,9 @@
  */
 package com.tiger.todolist.view;
 
+import com.tiger.todolist.controller.popupWindows;
 import java.awt.GridLayout;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
@@ -15,17 +17,68 @@ import javax.swing.JRadioButton;
  * @author k1732912
  */
 public class SubtaskBoard extends JPanel{
+
     private JLabel subTasks = new JLabel("Subtasks");
-    private JLabel taskName = new JLabel("Task Name");
-    private JRadioButton checkBox = new JRadioButton("Complete");;
-    
+    private JLabel taskName = new JLabel("");
+    private JLabel subTaskName = new JLabel();
+    private JCheckBox checkBox = new JCheckBox("Complete");
+    private popupWindows listener = new popupWindows();
     
     public SubtaskBoard(){
         this.setLayout(new GridLayout(8,4));
-        
         this.add(subTasks);
-        this.add(taskName);
-        this.add(checkBox);
+        String[] subTaskDetails = listener.getSubTaskDetails(listener.currentTask);
+ 
+        for (int i=0; i<subTaskDetails.length;i++){
+        String splitElements = subTaskDetails[i];
+        String[] splitDetails = splitElements.split(",");
+        if(this.taskName.getText().equals("")){
+            taskName.setText(splitDetails[0]);    
+            this.add(taskName);    
+        }
+        subTaskName.setText(splitDetails[1]);
+        this.add(subTaskName);
+        JCheckBox two = new JCheckBox();
+        two.setText("Complete");
+        two.setSelected(Boolean.valueOf(checkBox.getText()));
+        this.add(two);
+ 
         
+        }
+        
+      
+        
+    }
+    
+     public JLabel getSubTaskName() {
+        return subTaskName;
+    }
+
+    public void setSubTaskName(JLabel subTaskName) {
+        this.subTaskName = subTaskName;
+    }
+    
+     public JLabel getSubTasks() {
+        return subTasks;
+    }
+
+    public void setSubTasks(JLabel subTasks) {
+        this.subTasks = subTasks;
+    }
+
+    public JLabel getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(JLabel taskName) {
+        this.taskName = taskName;
+    }
+
+    public JCheckBox getCheckBox() {
+        return checkBox;
+    }
+
+    public void setCheckBox(JCheckBox checkBox) {
+        this.checkBox = checkBox;
     }
 }
